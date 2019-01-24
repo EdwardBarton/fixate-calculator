e8be1e1705a6c4ec75726a9c9441ae27bf1157a7
Edward Barton <edward.barton@icloud.com>
2019-01-24 13:55:42 -0500

    config deployment

--- src/app.js
+++ src/app.js
@@ -16,6 +16,19 @@ app.use(require('./routes/recipes'));
 app.use(require('./routes/users'));
 // app.use(require('./routes/generate-data')); // seed MongoDB
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/dist'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
